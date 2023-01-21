5920d45f734f3683076a8053b0724e9639b5470f
Omar-Codess <omartaki8@gmail.com>
2023-01-21 19:35:19 +0100

    aggiunti link alla pagina principale per HOME e ABOUT US

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,20 @@
 <script>
 export default {
-    name: 'AppHeader'
+    name: 'AppHeader',
+    data() {
+        return {
+            navigation: [
+                {
+                    label: 'Home',
+                    routeName: 'home'
+                },
+                {
+                    label: 'About us',
+                    routeName: 'about'
+                }
+            ]
+        }
+    }
 }
 </script>
 
@@ -8,10 +22,24 @@ export default {
     <header>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand text-primary" href="#">Boolfolio</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <a class="navbar-brand text-primary" href="#">BOOLPRESS</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item" v-for="(navItem, index) in navigation" :key="index">
+                        <router-link :to="{ name: navItem.routeName }" class="nav-link" aria-current="page">{{
+                            navItem.label
+                        }}</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="http://127.0.0.1:8000/admin">Login</a>
+                    </li>
+                </ul>
             </div>
         </nav>
     </header>
