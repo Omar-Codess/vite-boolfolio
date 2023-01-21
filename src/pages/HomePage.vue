3b480c79341e6ceac93b766d5bd69771dd54945b
Omar-Codess <omartaki8@gmail.com>
2023-01-21 17:03:19 +0100

    installato router e creata home page

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: 'HomePage'
+}
+</script>
+
+<template>
+    <div class="container">
+        <h2 class="mt-3 text-center">Home Page</h2>
+    </div>
+</template>
+
+<script>
+
+</script>
