6db6cb4c36c2434c7031ebe3863e169572eaf17b
Omar-Codess <omartaki8@gmail.com>
2023-01-24 09:01:24 +0100

    completata pagina singolo progetto

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -16,15 +16,24 @@ export default {
             if(resp.data.success) {
                 this.project = resp.data.project;
             } else {
-                this.$router.push({name: "not-found"})
+                this.$router.push({name: "not-found"});
             }
         });
+    },
+    computed: {
+        category() {
+            return this.project.category ? this.project.category.name :  'Nessuna categoria';
+        }
     }
 }
 </script>
 
 <template>
     <div class="container mt-3">
-        <h1 class="mt-3 text-center">Single Project</h1>
+        <h1 class="mt-3 text-center">{{ project.title }}</h1>
+        <h5 class="text-center mt-3 text-uppercase text-primary">{{ category }}</h5>
+        <img v-if="project.cover_image" :src="`${store.apiBaseUrl}/storage/${project.cover_image}`" alt="">
+        <div v-else class="text-center mt-2">Nessuna immagine trovata</div>
+        <p>{{ project.description }}</p>
     </div>
 </template>
